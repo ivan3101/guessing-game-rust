30df314eb29fd4667c0e76d1c168dd140ebf49ee
Ivan De Menezes <imenezes@aluxion.com>
2022-05-22 17:38:00 +0200

    feat: generate magic number

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 use std::io;
+use std::ops::{RangeInclusive};
+use rand::{Rng, thread_rng};
+
+const MAGIC_NUMBER_RANGE: RangeInclusive<i32> = 1..=50;
 
 fn main() {
-    println!("Welcome!");
+    let magic_number = generate_magic_number();
 
+    println!("Welcome!");
     println!("Please enter your guess");
 
     let mut guess = String::new();
@@ -12,4 +17,14 @@ fn main() {
         .expect("Error reading input");
 
     println!("You guessed {}", guess);
+
+    println!("The magic number is {}", magic_number);
+}
+
+fn generate_magic_number() -> i32 {
+    let mut rng = thread_rng();
+
+    let magic_number = rng.gen_range(MAGIC_NUMBER_RANGE);
+
+    magic_number
 }
