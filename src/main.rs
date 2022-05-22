5bca5c9870c5c45bf9fc8227a9f0d6b6ed22817b
Ivan De Menezes <imenezes@aluxion.com>
2022-05-22 17:28:02 +0200

    feat: read input from console

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!")
+    println!("Welcome!");
+
+    println!("Please enter your guess");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Error reading input");
+
+    println!("You guessed {}", guess);
 }
